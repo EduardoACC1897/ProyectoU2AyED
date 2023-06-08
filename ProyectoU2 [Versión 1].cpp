7b125d7cd8743666efc2773270bdf7b9b57ccb45
EduardoACC1897 <127953711+EduardoACC1897@users.noreply.github.com>
2023-06-08 17:44:31 -0400

    Add files via upload

---
+++ ProyectoU2 [Versión 1].cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+	
+	// Presentación y sinopsis del juego
+    cout << "******************************************************************** The Guardian Journey ******************************************************************" << endl;
+
+
+    cout << "\t\t\t\t\tEn un mundo devastado por la guerra y la lucha constante existen los Guardianes. Los" << endl
+         << "\t\t\t\t\tGuardianes son heroes con habilidades y destrezas unicas que protegen a las aldeas a lo" << endl
+         << "\t\t\t\t\tlargo de toda la tierra siendo estas los ultimos asentamientos de la humanidad." << endl << endl
+         << "\t\t\t\t\tTesla es la capital de estas tierras y hoy corresponde a un farol en la oscuridad donde" << endl
+         << "\t\t\t\t\tabunda la prosperidad, el desarrollo tecnologico y entregan la mayor proteccion a lo largo" << endl
+         << "\t\t\t\t\tdel nuevo mundo." << endl << endl
+         << "\t\t\t\t\tCada ser humano tiene un poder interior que le permite definir su destino y elegir en una" << endl
+         << "\t\t\t\t\tde las cuatro facciones que existen hoy en dia, las cuales son; magos, vikingos," << endl
+         << "\t\t\t\t\t\t\t\t\t    nigromantes y bestias. En ocasiones existen humanos con la posibilidad de desarrollar" << endl
+         << "\t\t\t\t\thabilidades de mas de una faccion lo que les permite ser grandes candidatos a recibir el" << endl
+         << "\t\t\t\t\ttitulo de guardianes." << endl << endl
+         << "\t\t\t\t\tA los alrededores de Tesla se encuentran las aldeas que corresponden a asentamientos" << endl
+         << "\t\t\t\t\tque cumplen distintas funciones dentro de la gran organizacion de la humanidad y en cada" << endl
+         << "\t\t\t\t\tuna de estas aldeas existen maestros, quienes enseñan las artes de los guardianes." << endl<< endl
+         << "\t\t\t\t\tEl sueno de cada nino es convertirse en un Guardian y ganar el torneo, pero el viaje de un" << endl
+         << "\t\t\t\t\taprendiz no es facil y su destino depende de las decisiones que tome a lo largo de este" << endl
+         << "\t\t\t\t\trecorrido." << endl << endl;
+	
+	cout << "\t\t\t\t\t";
+	
+    system("pause");
+    
+    return 0;
+    
+}
